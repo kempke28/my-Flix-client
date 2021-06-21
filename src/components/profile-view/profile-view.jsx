03470ab11ec645a08a9454af4c76eff64d3a02e9
Akempke <akempkedev@gmail.com>
2021-06-21 03:25:01 +0100

    Another final push

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -6,7 +6,17 @@ import axios from 'axios';
 import "../profile-view/profile-view.scss";
 
 export class ProfileView extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      userName :"",
+      password: "",
+      birthday: "",
+      email: ""
 
+    };
+  }
 
 
 
@@ -27,8 +37,43 @@ export class ProfileView extends React.Component {
     
   }
 
+  updateUser(user, token)
+  {
+    axios.put(`https://movie-api-1.herokuapp.com/users/${user}`,
+    {
+      Username: this.state.userName,
+      Password: this.state.password,
+      Email: this.state.email,
+      Birthday: this.state.birthday,
+    },
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+    .then((response) => {
+      const data = response.data;
+      localStorage.setItem("user", data.Username);
+
+      let userData = JSON.parse(localStorage.getItem('userData'));
+      userData.Birthday = data.Birthday;
+      userData.Email = data.Email;
+      userData.Password = data.Password;
+      localStorage.setItem('userData', JSON.stringify(userData))
+    })
+    .catch(function (error) {
+      console.log(error.response.data);
+    });
+  }
+  
+
+  handleChange(e) {
+    let { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
     render() {  
       const { user, onBackClick, token} = this.props;
+
       return(
         <div>
           <div className="user">
@@ -49,6 +94,26 @@ export class ProfileView extends React.Component {
           <div className="user">
           <span className="label">Day born: </span>
           <span className="value">{user.Birthday}</span>
+          </div>
+
+          <div className="user">
+          <span className="label">user: </span>
+          <input className="value" name="userName" type="text" value={this.state.userName} onChange={(e) => this.handleChange(e)}/>
+          </div>
+
+          <div className="user">
+          <span className="label">password: </span>
+          <input className="value" name="password" type="text" value={this.state.password} onChange={(e) => this.handleChange(e)}/>
+          </div>
+
+          <div className="user">
+          <span className="label">email: </span>
+          <input className="value" name="email"  type="text" value={this.state.email} onChange={(e) => this.handleChange(e)}/>
+          </div>
+
+          <div className="user">
+          <span className="label">Day born: </span>
+          <input className="value" name="birthday" type="date" value={this.state.birthday} onChange={(e) => this.handleChange(e)}/>
         </div>
 
         <div>
@@ -59,6 +124,7 @@ export class ProfileView extends React.Component {
           </ul>
         </div>
           <Button variant="info" className="my-3" onClick={()=>this.deregister(user.Username, token)}>Delete account</Button>
+          <Button variant="info" className="my-3" onClick={()=>this.updateUser(user.Username, token)}>Update User</Button>
           <Button variant="info" className="my-3" onClick={()=>onBackClick()}>Back</Button>
 
       </div>
